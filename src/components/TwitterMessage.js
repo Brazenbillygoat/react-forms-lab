f105cf211e7f75e7b298432a3053fb4df8c1d682
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-11-05 23:25:04 -0500

    Forms now controlled by react in state

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,28 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  updateMessage = (message) => {
+    this.setState({message})
   }
 
   render() {
+    let remainingChars = this.props.maxChars - this.state.message.length
+    
+
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" 
+               name="message" 
+               id="message" 
+               value={this.state.message} 
+               onChange={(e) => this.updateMessage(e.target.value)} />
+        {remainingChars}
       </div>
     );
   }
